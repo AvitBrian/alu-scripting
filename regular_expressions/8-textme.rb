e0653dd2ed36f7feb9267edc7612dd2994c1d81c
AvitBrian <b.mugisha2@alustudent.com>
2023-01-11 08:06:11 -0800

    almost done?

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -3,4 +3,4 @@ text = ARGV[0]
 sender = text.scan(/(?<=from:).[[a-z|+?0-9]]*/i)
 receiver = text.scan(/(?<=to:).[[a-z]+?0-9]*/i)
 flags = text.scan(/(?<=flags).[-?\d+[:]-?\d]*/)
-puts "#{sender},#{receiver},#{flags}"
+puts "#{sender[0]},#{receiver[0]},#{flags[0]}"
