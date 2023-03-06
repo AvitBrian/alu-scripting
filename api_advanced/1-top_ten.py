b5d13b6a935c4ccb3a2b4009d3612d9f77f92591
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2023-03-06 12:30:36 +0000

    updates

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""
+this module contains a function that queries the Reddit API 
+and prints the titles of the first 10 hot posts listed for a given subreddit.
+:param subreddit: string, the subreddit to search for hot posts.
+"""
 import requests
-import json
+
 
 def top_ten(subreddit):
     """
-        fetches the top 10 hot posts.
+        fetches the top 10 hot posts listed for a given subreddit.
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {
@@ -15,6 +20,6 @@ def top_ten(subreddit):
     if response.status_code == 200:
         data = response.json()["data"]["children"]
         for i in range(10):
-            return (data[i]["data"]["title"])
-    else:return ("None")
-    
+            return data[i]["data"]["title"]
+    else:
+        return "None"
--- api_advanced/3-count.py
+++ api_advanced/3-count.py
@@ -1,60 +1,47 @@
 #!/usr/bin/python3
-import requests
-
-def count_words(subreddit, word_list, after=None, count_dict=None):
-    """
-    Recursively queries the Reddit API, parses the title of all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces. Javascript should count as javascript, but java should not).
-
+"""
+    Recursively queries the Reddit API, parses the title of all hot articles, 
+    and prints a sorted count of given keywords
+    (case-insensitive, delimited by spaces. 
+    Javascript should count as javascript, but java should not).
     :param subreddit: string, the subreddit to search for
     :param word_list: list of strings, the keywords to count
     :param after: string, the value of the 'after' parameter in the previous API call
     :param count_dict: dictionary, a dictionary to store the counts of each keyword
     :return: None
+"""
+import requests
+
+
+
+def count_words(subreddit, word_list, after=None, counts={}):
     """
-
-    # Base case: if count_dict is None, initialize it
-    if count_dict is None:
-        count_dict = {}
-
-    # Base case: if word_list is empty, print the count_dict
-    if not word_list:
-        for word, count in sorted(count_dict.items(), key=lambda x: (-x[1], x[0])):
-            print(f"{word}: {count}")
-        return
-
-    # Get the data from the Reddit API
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+        Recursively queries the Reddit API and passes the results to a helper function.
+    """
     headers = {'User-Agent': 'mybot/0.0.1'}
-    params = {'limit': '100', 'after': after}
-    response = requests.get(url, headers=headers, params=params)
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=100'
+    
+    if after is None:
+        counts = {}
 
-    # If the API call was unsuccessful, print an error message and return None
+    if after:
+        url += f'&after={after}'
+    response = requests.get(url, headers=headers, timeout=10)
+    
     if response.status_code != 200:
-        print(f"Error: status code {response.status_code}")
-        return
-
-    # Get the JSON data from the API response
+        return None
     data = response.json()
-
-    # Get the next 'after' parameter, which is used for pagination
-    after = data['data']['after']
-
-    # Loop through the posts in the data
     for post in data['data']['children']:
-        # Get the lowercase title of the post
-        title = post['data']['title'].lower()
-        # Loop through the words in word_list
+        title = post['data']['title']
+        words = title.lower().split()
         for word in word_list:
-            # Get the lowercase version of the word without any punctuation at the end
-            word_lower = word.rstrip('.!_').lower()
-            # Check if the word is in the title
-            if f" {word_lower} " in f" {title} ":
-                # If the word is in the title, add it to the count_dict
-                if word_lower in count_dict:
-                    count_dict[word_lower] += 1
-                else:
-                    count_dict[word_lower] = 1
-
-    # Recursively call count_words with the updated parameters
-    count_words(subreddit, word_list, after=after, count_dict=count_dict)
+            word = word.lower()
+            if word in words:
+                counts[word] = counts.get(word, 0) + words.count(word)
+    after = data['data']['after']
+    if after is None:
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for count in sorted_counts:
+            print(f'{count[0]}: {count[1]}')
+        return
+    count_words(subreddit, word_list, after=after, counts=counts)
