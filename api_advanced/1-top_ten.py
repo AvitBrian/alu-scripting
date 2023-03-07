a921acc134ed9b8c2eb4be5aaae76007b5e5b19b
AvitBrian <113444617+AvitBrian@users.noreply.github.com>
2023-03-07 15:37:28 +0000

    updates here

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 """
+
 this module contains a function that queries the Reddit API 
 and prints the titles of the first 10 hot posts listed for a given subreddit.
-:param subreddit: string, the subreddit to search for hot posts.
+
 """
+
 import requests
 
 
